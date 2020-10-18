76d0664e19be1d5e834fd341ec3a88dc95de762e
sniqq1 <48368678+sniqq1@users.noreply.github.com>
2020-10-18 17:24:20 +0200

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,25 @@
+// zadanie 1 
+let a = 2;
+let b ='dwa';
+let c = a+b;
+let d = false;
+let e = null;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+// zadanie 2 
+let q = 4;
+let w = 6;
+let sum =0;
+sum = q+w;
+console.log(sum);
+
+// zadanie 3
+
+let r;
+console.log(r);
+// undefined
