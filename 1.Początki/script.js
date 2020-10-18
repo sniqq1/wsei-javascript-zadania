90f504b68f62ce5c7d40eef4cdf0f21899c0de84
sniqq1 <48368678+sniqq1@users.noreply.github.com>
2020-10-18 17:04:43 +0200

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//Zadanie 1:
+console.log("Patryk");
+
+//Zadanie 2
+console.log(12+32);
+console.log(52/16); // zmiennoprzecinkowa
+console.log(5*2);
+
+//Zadanie 3
+console.log("Hello World");
+//console.log(Hello World); // bład, nie znana zmienna
+// console.log("Hello World); //bład, nie zakończony string
