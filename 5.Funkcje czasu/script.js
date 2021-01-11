e02c612621ecefd60c691f68c18632ee51374857
sniqq1 <48368678+sniqq1@users.noreply.github.com>
2021-01-12 00:25:16 +0100

    Update script.js

--- 5.Funkcje czasu/script.js
+++ 5.Funkcje czasu/script.js
@@ -13,3 +13,22 @@ const Hello = () => {
   counter++;
 }
 setInterval(Hello,3000);
+
+
+// zadanie 2
+
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+setTimeout(function () {
+  console.log(arr);
+  let a = 0;
+ let b = setInterval(function () {
+    if (a<arr.length){
+      console.log(arr[a]);
+      a++;
+    }
+   else{
+     clearInterval(b);
+   }    
+  },3000);
+},2000);
