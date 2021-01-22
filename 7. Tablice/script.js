67f5cda7a14b1d94537c63ed57cbbf9e660a0119
sniqq1 <48368678+sniqq1@users.noreply.github.com>
2021-01-22 12:37:44 +0100

    Update script.js

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -13,3 +13,49 @@ console.log(a.filter((x,i) =>{
 console.log(a.filter((x,i) => {
   return typeof(x)=="number"
 }))
+
+// zadanie 3
+var a = [1,2,3,4,5,10,20,22,34];
+let sum=0;
+for (let i = 0; i<a.length; i++) {
+  sum += a[i];
+}
+console.log(sum);
+
+let min=0;
+for (let i = 0; i<a.length; i++) {
+  min -= a[i];
+}
+console.log(min);
+
+let sum=0;
+for (let i = 0; i<a.length; i++) {
+  sum += a[i];
+}
+let sr = sum/a.length;
+console.log(sr);
+
+for (let i = 0; i<a.length; i++) {
+  if (a[i] %2==0) console.log(a[i]);
+}
+
+for (let i = 0; i<a.length; i++) {
+  if (a[i] %2!=0) console.log(a[i]);
+}
+
+let temp= Number.MIN_VALUE;
+for (let i = 0; i<a.length; i++) {
+ if(a[i] > temp) temp=a[i]; 
+}
+console.log(temp);
+
+var a = [1,2,3,4,5,10,20,34,22];
+let temp= Number.MAX_VALUE;
+for (let i = 0; i<a.length; i++) {
+ if(a[i] < temp) temp=a[i]; 
+}
+console.log(temp);
+
+
+console.log(a.reverse());
+
