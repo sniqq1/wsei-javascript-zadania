27e6c55e54550f986b298657daedb0b656dbf2c1
sniqq1 <48368678+sniqq1@users.noreply.github.com>
2021-02-02 09:56:54 +0100

    Update script.js

---
+++ 9.Konstruktory i prototype/script.js
@@ -0,0 +1,29 @@
+// zadanie 1
+class Person {
+  constructor(imie, nazwisko, wiek, kraj, miasto, jezyk) {
+    this.imie = imie;
+    this.nazwisko = nazwisko;
+    this.wiek = wiek;
+    this.kraj = kraj;
+    this.miasto = miasto;
+    this.jezyk = jezyk;
+  } 
+  zmienWiek(wiek) {
+    this.wiek = wiek;
+  }
+  zmienMiasto(miasto) {
+    this.miasto = miasto;
+  }
+}
+  const a = new Person('Patryk','Wojtaszek',28,'Polska','Kraków','polski');
+const b = new Person('Adam','Małysz',60,'Polska','Wisła','polski');
+const c = new Person('Donald','Trump',80,'USA','Waszyngton','angielski');
+const d = new Person('Jacek','Sasin',50,'Polska','Warszawa','polski');
+const e = new Person('Natalia','Oreiro',18,'Kolumbia','Montevideo','hiszpański');
+
+e.zmienWiek(50);
+c.zmienMiasto('sybir');
+d.zmienMiasto('Tarnów');
+console.log(c);
+console.log(d);
+console.log(e);
