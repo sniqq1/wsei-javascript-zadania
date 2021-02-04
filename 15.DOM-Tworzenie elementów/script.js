7c7383b461b696b4499b762edb5eacfd30835809
sniqq1 <48368678+sniqq1@users.noreply.github.com>
2021-02-04 16:05:41 +0100

    Update script.js

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -40,3 +40,26 @@ for (let i=0; i<divNumb; i++) {
 
 
 //zadanie6
+const obj = {
+  div1: 'to jest div', span1: 'to jest span', div2: { div3: 'to jest div', }, span2: 'to jest span',
+}
+
+const firstDiv = document.createElement('div');
+firstDiv.innerText = obj.div1;
+const firstSpan = document.createElement('span');
+firstSpan.innerText = obj.span1;
+
+firstDiv.append(firstSpan); 
+document.body.append(firstDiv, document.getElementById('root'));
+
+const secondDiv = document.createElement('div');
+const thirdDiv = document.createElement('div');
+thirdDiv.innerText = obj.div2.div3;
+
+const secondSpan = document.createElement('span');
+secondSpan.innerText = obj.span2;
+
+secondDiv.append(thirdDiv);
+secondDiv.append(secondSpan);
+
+document.body.append(secondDiv, document.getElementById('root'));
