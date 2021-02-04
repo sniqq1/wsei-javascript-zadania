b5f0fa5d1540d2dd861353f771b5d67dc4ce6a29
sniqq1 <48368678+sniqq1@users.noreply.github.com>
2021-02-04 13:34:47 +0100

    Update script.js

--- 14.DOM-elementy/script.js
+++ 14.DOM-elementy/script.js
@@ -43,3 +43,63 @@ document.getElementById("spanText").innerText =  "dowolny";
 document.getElementById("spanText").className =  "dowolna";
 
 //zadanie 6
+
+function zadanie6 (classes) {
+  classes.forEach(i => console.log(i));
+  console.log([...classes].join('+'));
+  document.getElementsByClassName(classes.value).classList=[];
+  console.log('Usunięto wszytskie klasy');
+}
+zadanie6(document.getElementById('classes').classList);
+
+//zadanie7 
+  var zadanie7 = document.querySelector('#longList').querySelectorAll('li');
+  zadanie7.forEach((c) => {
+    if(c.dataset.text == null) c.setAttribute("data-text", "text");
+  });
+
+//zadanie8
+function saveString(string_z_parametru){
+    const obj = {
+        newClass: string_z_parametru
+    }
+    return obj;
+}
+
+function loadString(obj) {
+    var x = obj.newClass;
+    document.getElementById('myDiv').classList.add(x);
+}
+loadString(saveString('string_z_parametru'));
+
+//zadanie 9
+function addClass (number) {
+    var newClass = number%2==0 ? 'even' : 'odd';
+    console.log(number, newClass);
+    document.getElementById('numbers').classList.add(newClass);
+}
+var random = Math.round(Math.random()*10);
+addClass(random);
+
+//zadanie 10
+    var listValues = document.querySelector('#longList');
+    
+    function showTab(listValues) {
+        var li = [];
+        listValues.querySelectorAll('li').forEach((d) => {
+            li.push(d.innerHTML);
+        });
+        return li;
+    }
+
+    console.log(showTab(listValues));
+
+//zadanie 11
+function changeValues(el) {
+  [...el.children].forEach(v => {
+    v.setAttribute('old-value', v.innerText);
+    v.innerText = Math.floor(Math.random() * 10);
+  });
+}
+
+changeValues(document.getElementById('longList'));
